826faade258f51de8c11cc089d2e76e1513d05f7
kevinzou369 <kevinzouyushan@gmail.com>
2021-10-26 00:51:36 +0800

    Level 3. Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,11 +11,14 @@ public class Duke {
         System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
 
         String dataIn;
+        int index = 0;
+        String temp;
         int count = 0;
         String arry[];
         arry = new String[100];
         String exit = "bye";
         String show = "list";
+        String done = "done";
         Scanner scan = new Scanner(System.in);
         for (int i = 0; i < arry.length; i++) {
             dataIn = scan.nextLine();
@@ -23,13 +26,19 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }else if (dataIn.equals(show)){
+                System.out.println("Here are the tasks in your list:");
                 for (int j = 0; j < count; j++){
-                    System.out.println(j + 1 + ". " + arry[j]);
+                    System.out.println(j + 1 + "." + arry[j]);
                 }
+            }else if (dataIn.contains(done)) {
+                Integer num = Integer.parseInt(dataIn.replaceAll("[\\D]",""));
+                System.out.println("Nice! I've marked this task as done:");
+                arry[num-1] = arry[num-1].substring(0,1) + "X" + arry[num-1].substring(2);
+                System.out.println("  " + arry[num-1]);
             }else{
-                arry[i] = dataIn;
+                arry[i] = "[ ] " + dataIn;
                 count++;
-                System.out.println("added: " + arry[i]);
+                System.out.println("added: " + arry[i].substring(4));
             }
         }
     }
