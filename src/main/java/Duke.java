8b0e6ca826f48fb6d55348be7004039868fb7f86
kevinzou369 <kevinzouyushan@gmail.com>
2021-11-08 23:04:14 +0800

    Level 5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,43 +38,60 @@ public class Duke {
                     System.out.println(j + 1 + "." + arry.get(j).toString().substring(0,3) + "[" + arry.get(j).getStatusIcon() + "] " + arry.get(j).toString().substring(7));
                 }
             }else if (dataIn.contains(done)) {
-                Integer num = Integer.parseInt(dataIn.replaceAll("[\\D]",""));
-                arry.get(num-1).markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                //arry[num-1] = arry[num-1].substring(0,4) + "X" + arry[num-1].substring(5);
-                //System.out.println("  " + arry[num-1].substring(3));
-                System.out.println("  [" + arry.get(num-1).getStatusIcon() + "] " + arry.get(num-1).getDescription());
+                try {
+                    Integer num = Integer.parseInt(dataIn.replaceAll("[\\D]", ""));
+                    arry.get(num - 1).markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    //arry[num-1] = arry[num-1].substring(0,4) + "X" + arry[num-1].substring(5);
+                    //System.out.println("  " + arry[num-1].substring(3));
+                    System.out.println("  [" + arry.get(num - 1).getStatusIcon() + "] " + arry.get(num - 1).getDescription());
+                } catch (Exception e) {
+                    System.out.println("☹ OOPS!!! The index of a done cannot be empty.");
+                }
             }else if (dataIn.contains(todo)) {
-                Task t = new Todo(dataIn.substring(5));
-                arry.add(t);
-                count++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + arry.get(count-1).toString());
-                System.out.println("Now you have " + arry.size() + " tasks in the list.");
+                try {
+                    Task t = new Todo(dataIn.substring(5));
+                    arry.add(t);
+                    count++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + arry.get(count - 1).toString());
+                    System.out.println("Now you have " + arry.size() + " tasks in the list.");
+                } catch (Exception e) {
+                    System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+                }
             }else if (dataIn.contains(deadline)) {
-                int idx = dataIn.indexOf("/");
-                Task t = new Deadline(dataIn.substring(9,idx-1), dataIn.substring(idx+4));
-                arry.add(t);
-                count++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + arry.get(count-1).toString());
-                System.out.println("Now you have " + arry.size() + " tasks in the list.");
+                try {
+                    int idx = dataIn.indexOf("/");
+                    Task t = new Deadline(dataIn.substring(9, idx - 1), dataIn.substring(idx + 4));
+                    arry.add(t);
+                    count++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + arry.get(count - 1).toString());
+                    System.out.println("Now you have " + arry.size() + " tasks in the list.");
+                } catch (Exception e) {
+                    System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+                }
             }else if (dataIn.contains(event)) {
-                int idx = dataIn.indexOf("/");
-                Task t = new Event(dataIn.substring(6,idx-1), dataIn.substring(idx+4));
-                arry.add(t);
-                count++;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + arry.get(count-1).toString());
-                System.out.println("Now you have " + arry.size() + " tasks in the list.");
+                try {
+                    int idx = dataIn.indexOf("/");
+                    Task t = new Event(dataIn.substring(6, idx - 1), dataIn.substring(idx + 4));
+                    arry.add(t);
+                    count++;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + arry.get(count - 1).toString());
+                    System.out.println("Now you have " + arry.size() + " tasks in the list.");
+                } catch (Exception e) {
+                    System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
+                }
             }else{
                 //arry[i] = "[ ][ ] " + dataIn;
                 //count++;
                 //System.out.println("added: " + arry[i].substring(7));
-                Task t = new Task(dataIn);
-                arry.add(t);
-                count++;
-                System.out.println("added: " + arry.get(arry.size()-1).getDescription());
+                //Task t = new Task(dataIn);
+                //arry.add(t);
+                //count++;
+                //System.out.println("added: " + arry.get(arry.size()-1).getDescription());
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }while(!dataIn.equals(exit));
     }
